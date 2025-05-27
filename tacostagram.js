82aebbdc2e5ad99868560b946fab8e920d567ff7
Stephen McCaffery <stephen.mccaffery@kellogg.northwestern.edu>
2025-05-27 16:23:35 +0000

    end of class

--- tacostagram.js
+++ tacostagram.js
@@ -1,30 +1,42 @@
 function getPostsFromAPI() {
   // Replace this with the URL of the JSON API that returns an array of image URLs
-  const url = 'YOUR_URL_GOES_HERE';
+  const url = 'https://3000-mccafferys19-css-dqw5k4g3t7a.ws-us119.gitpod.io/posts.json';
   if (url == 'YOUR_URL_GOES_HERE') {
     alert('Error: Replace url value in tacostagram.js')
   }
 
-  // Make a GET request to the API
+// Make a GET request to the API
   fetch(url)
-    .then(response => response.json()) // Parse the response as JSON
+    .then(response => response.json())
     .then(posts => {
-      // Loop through the array of posts and build html for each
+      const postsDiv = document.querySelector('#posts');
+      postsDiv.innerHTML = ''; // clear previous posts
+
       for (let post of posts) {
-        // Log post data to browser console
         console.log(post);
 
-        // html for each post goes here
-        let html = ``;
+        // Fallback for anonymous user
+        const userName = post.user && post.user.first_name ? post.user.first_name : "anonymous";
 
-        const postsDiv = document.querySelector('#posts');
-        postsDiv.innerHTML += html
-      };
+        let html = `
+          <div class="col-12 col-md-6 col-lg-4 col-xl-3 col-xxl-2 mb-4">
+            <div class="card">
+              <img src="${post.image}" class="card-img-top img-fluid" alt="Taco Image">
+              <div class="card-body">
+                <h5 class="card-title">${userName}</h5>
+                <p class="card-text">${post.body}</p>
+              </div>
+            </div>
+          </div>
+        `;
+
+        postsDiv.innerHTML += html;
+      }
     })
     .catch(error => {
       console.error('Error fetching posts:', error);
     });
-}
+  }
 
 const getPostsButton = document.querySelector('#get-posts-button');
 getPostsButton.addEventListener('click', getPostsFromAPI);
